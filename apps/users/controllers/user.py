25b878bbdee1186d942843d6e4ebdf62d3bbec8f
Aayush Kurup <aayushkurup786@outlook.com>
2023-02-02 00:28:22 +0530

    ADD: run script plugin for poetry

--- apps/users/controllers/user.py
+++ apps/users/controllers/user.py
@@ -1,4 +1,4 @@
-from typing import Dict, TYPE_CHECKING
+from typing import Dict
 from fastapi import Depends
 from dependency_injector.wiring import inject, Provide
 from pkgs.core import create_router
@@ -9,7 +9,7 @@ from ..services import UserService
 router = create_router('/api/v1')
 router.default_response_class = HTTPResponse
 
-@router.get('/users')
+@router.post('/users')
 @inject
 def get_users(user_service: UserService = Depends(Provide[UserContainer.user_service])) -> Dict:
     return user_service.greet()

97f352e7a038547cafe3164384b2426c56507ca7
Aayush Kurup <aayushkurup786@outlook.com>
2023-02-08 13:13:41 +0530

    ADD: exception handler class

--- pkgs/core/http/create_router.py
+++ pkgs/core/http/create_router.py
@@ -1,4 +1,10 @@
 from fastapi import APIRouter
+from .response import HTTPResponse 
+from .exception import ExceptionHandler
 
 def create_router(route_prefix: str):
-    return APIRouter(prefix=route_prefix)
+    router = APIRouter(prefix=route_prefix)
+    router.route_class = ExceptionHandler
+    router.default_response_class = HTTPResponse
+    
+    return router

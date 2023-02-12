487a4de61448c93e8ce157abacd8f461641e9881
Aayush Kurup <aayushkurup786@outlook.com>
2023-02-12 21:11:40 +0530

    FIX: value accessed in loop

--- config/utils/resolve.py
+++ config/utils/resolve.py
@@ -3,13 +3,13 @@ from .. import config
 
 def resolve(key: str):
     pattern = r'(\w+.\w+)*(\.\w+)+'
-    if not re.match(pattern):
+    if not re.match(pattern, key):
         raise ValueError("Invalid key")
     
     keys = key.split('.')
     value = config.get(keys.pop(0))
     for key in keys:
-        value = config.get(key)
+        value = value.get(key)
         if not value:
             return None
         
